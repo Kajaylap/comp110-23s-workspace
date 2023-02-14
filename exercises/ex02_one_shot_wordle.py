142485886f97dd32000120ce20fd3a531110171c
Kajayla <kajayla12p@gmail.com>
2023-02-13 22:15:05 -0500

    almost complete

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,33 +2,37 @@
 
 __author__ = "730418782"
 
-guess = input("What is your 6-letter guess? ")
 SECRET_WORD = "python"
+len_secret_word: int = len(SECRET_WORD)
+guess = input(f"What is your { len_secret_word }-letter guess? ")
 guessing: bool = True
 guess_index: int = 0
-guess_character: bool = False
 alt_indices: int = 0
+guess_character: bool = False
+
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
-yellow_box = str = "\U0001F7E8"
+yellow_box: str = "\U0001F7E8"
+
+result = " " 
 while guessing:
-    if len(guess) != len(SECRET_WORD): # the input doesn't equal the secret word
+    while len(guess) != len_secret_word: # the input doesn't equal the secret word
         print("That was not 6 letters! Try again: ")
-        guess = input("What is your 6-letter guess? ")
-    else:
-        while len(SECRET_WORD) > guess_index: # the while loops repeat until 
-            # guess_index is greater than the len of the secret word
-            if guess[guess_index] == SECRET_WORD[guess_index]:
-                print(green_box)
-            else:
-                while guess_character:
-                    if len(SECRET_WORD) > guess_character:
-                        if guess[alt_indices] == SECRET_WORD[alt_indices]:
-                            guess_character = True
+        guess = input(f"What is your { len_secret_word }-letter guess? ")
+    while len_secret_word > guess_index: # the while loops repeat until 
+        # guess_index is greater than the len of the secret word
+        if guess[guess_index] == SECRET_WORD[guess_index]:
+            result = result + green_box #concatenating a string for the boxes
+        else:
+            while guess_character:
+                while len_secret_word > alt_indices:
+                    if guess[alt_indices] == SECRET_WORD[alt_indices]:
+                        guess_character = True
+                    else:
+                        result = result + white_box
                         alt_indices = alt_indices + 1
-                    print(yellow_box)
-                print(white_box)
-            guess_index = guess_index + 1
-        
-# Need to make while loop false to exit
+            result = result + yellow_box
+            alt_indices = alt_indices + 1
+    guessing = False
+    print(result)
 
