ba7fea9c2d15f530a51113cde48d12feeb4fd6ee
Kajayla <kajayla12p@gmail.com>
2023-02-13 22:37:48 -0500

    yellow box

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,13 +8,10 @@ guess = input(f"What is your { len_secret_word }-letter guess? ")
 guessing: bool = True
 guess_index: int = 0
 alt_indices: int = 0
-guess_character: bool = False
-
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
-
-result = " " 
+result: str = "" 
 while guessing:
     while len(guess) != len_secret_word: # the input doesn't equal the secret word
         print("That was not 6 letters! Try again: ")
@@ -24,6 +21,7 @@ while guessing:
         if guess[guess_index] == SECRET_WORD[guess_index]:
             result = result + green_box #concatenating a string for the boxes
         else:
+            guess_character: bool = False
             while guess_character:
                 while len_secret_word > alt_indices:
                     if guess[alt_indices] == SECRET_WORD[alt_indices]:
@@ -33,6 +31,6 @@ while guessing:
                         alt_indices = alt_indices + 1
             result = result + yellow_box
             alt_indices = alt_indices + 1
-    guessing = False
-    print(result)
+            guessing = False
+        print(result)
 
