eb680bc2c6df95f16612b97aa43c0769826ab640
Kajayla <kajayla12p@gmail.com>
2023-02-15 23:20:27 -0500

    done

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -16,23 +16,23 @@ result: str = ""
 
 
 while guessing:
-    while len(guess) != len_secret_word:  #the input doesn't equal the secret word
+    while len(guess) != len_secret_word:  
         print("That was not 6 letters! Try again: ")
         guess = input(f"What is your { len_secret_word }-letter guess? ")
 
-    while len_secret_word > guess_index:   #the while loops repeat until guess_index is greater than the len of the secret word
+    while len_secret_word > guess_index:  
         if guess[guess_index] == SECRET_WORD[guess_index]:
-            result = result + green_box  #concatenating a string for the boxes  
+            result = result + green_box    
             guess_index = guess_index + int(1)
         else:
             guess_character: bool = False
             alt_indices: int = 0
             while not guess_character and alt_indices < len_secret_word:
-                    if guess[guess_index] == SECRET_WORD[alt_indices]:
-                        guess_character = True
-                    else:
-                        alt_indices = alt_indices + 1
-            if guess_character == True:
+                if guess[guess_index] == SECRET_WORD[alt_indices]:
+                    guess_character = True
+                else:
+                    alt_indices = alt_indices + 1
+            if guess_character is True:
                 result = result + yellow_box
             else:
                 result = result + white_box
@@ -43,10 +43,10 @@ print(result)
 result_text: bool = True
 result_index: int = 0
 while result_text:
-       result_index = result_index + 1
-       if result[result_index] == green_box:
-            print("Woo! You got it!")
-            result_text = False
-       else:
-           print("Not quite. Play again soon!")
-           result_text = False
+    result_index = result_index + 1
+    if result[result_index] == green_box:
+        print("Woo! You got it!")
+        result_text = False
+    else:
+        print("Not quite. Play again soon!")
+        result_text = False
