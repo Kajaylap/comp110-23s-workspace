99d456782d301a0f95c543d6217dcf707a6dfd83
Kajayla <kajayla12p@gmail.com>
2023-04-18 11:20:06 -0400

    update

---
+++ exercises/ex09/river_simulation.py
@@ -0,0 +1,13 @@
+from exercises.ex09.river import River
+
+
+my_river = River(10,2)
+
+
+def view_river(self) -> None:
+    print(f"~~~ Day, {self.day}, :")
+    print(f"Fish population: , {self.fish}")
+    print(f"Bear population: , {self.bears}")
+    return None
+
+my_river.view_river
